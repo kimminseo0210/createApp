fde01e53129409ace90a90fb540cdf5130895736
kimminseo0210 <kimminseo@gimminseoui-MacBookAir.local>
2025-03-14 10:52:14 +0900

    25.03.13 - change backgroundColor

--- lib/main.dart
+++ lib/main.dart
@@ -10,38 +10,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // App의 배경색 지정
       theme: ThemeData(
-        // 앱의 기본 색상 정의
-        primaryColor: Colors.deepPurpleAccent, // 주 색상 (AppBar, 버튼 등)
-        primarySwatch: Colors.deepPurple, // 주 색상 팔레트
-        scaffoldBackgroundColor: Color.fromRGBO(161, 64, 64, 1.0), // Scaffold의 기본 배경색
-
-        // AppBar 테마
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.white, // AppBar 배경색
-          toolbarHeight: 30, // AppBar 높이
-          titleTextStyle: TextStyle(color: Colors.black), // AppBar 텍스트 색상
-          iconTheme: IconThemeData(color: Colors.black), // AppBar 아이콘 색상
+        // Color : Space Gray
+        appBarTheme: AppBarTheme(
+          backgroundColor: const Color(0xFF4B4B4B),
         ),
-
-        // BottomAppBar 테마
+        scaffoldBackgroundColor: const Color(0xFF4B4B4B),
         bottomAppBarTheme: const BottomAppBarTheme(
-          color: Colors.deepPurpleAccent, // BottomAppBar 배경색
-        ),
+          color: Color(0xFF4B4B4B),
+        )
 
-        // 그 외 위젯 테마 설정 가능
       ),
       home: Scaffold(
         appBar: AppBar(
-          // 이제 AppBar의 색상은 ThemeData에서 정의된 색상을 따릅니다.
+
         ),
         body: ListView(
-          children: [
-            // 리스트 뷰
-          ],
+
         ),
-        bottomNavigationBar: const BottomAppBar(
-          // 이제 BottomAppBar의 색상은 ThemeData에서 정의된 색상을 따릅니다.
+        bottomNavigationBar: BottomAppBar(
+
         ),
       ),
     );
